5736cef542a3a8e95693448822f416fc0ca0896a
Kassandra Flores <kflores418@gmail.com>
2026-04-07 22:11:16 -0700

    HW Done :)

--- ArraysAssignment2.cpp
+++ ArraysAssignment2.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 // Function prototype
 int countPerfect(const int scores[], int size);
 
-int main() {
+int main() 
+{
     int scores[20];
     int count = 0;
     int input;
-
     cout << "Enter up to 20 scores (0-100). Enter -1 to stop:\n";
+    cin >> input;
 
-    // Store scores and keep track of how many are entered
+    while(count < 20)
+    {
+        if (input <= 100 && input >= 0)
+        {
+        scores[count] = input;
+        count++;
+        //cout << "Next Score: ";
+        //cin >> input;
+        }
+        else if (input == -1)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Invalid Score";
+            break;
+        }
+        cout << "Next Score: ";
+        cin >> input;
+    }
+    
     // Report perfect scores using the helper function
-  // COMPLETE THE FUNCTION BELOW
+    // COMPLETE THE FUNCTION BELOW
   int perfectOnes = countPerfect(scores, count);
     
     cout << "\nYou entered " << count << " scores." << endl;
@@ -26,6 +49,13 @@ int main() {
 // Value-returning function to count scores of 100
 int countPerfect(const int scores[], int size) {
     int totalPerfect = 0;
-    //COMPLETE THE CODE 
+    
+    for (int i = 0; i < size; i++)
+    {
+        if (scores[i] == 100)
+        {
+            totalPerfect++;
+        }
+    }
     return totalPerfect;
 }
